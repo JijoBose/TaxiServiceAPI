21898e930189faab73e56cd6fd6be74225049a18
Jijo Bose <bosejijo@gmail.com>
2019-12-15 18:57:00 +0530

    ride model

--- app/controllers/api/v1/rides_controller.rb
+++ app/controllers/api/v1/rides_controller.rb
@@ -1,74 +1,40 @@
 class Api::V1::RidesController < ApplicationController
-  before_action :set_api_v1_ride, only: [:show, :edit, :update, :destroy]
+  before_action :set_ride, only: [:show, :edit, :update, :destroy]
 
   # GET /api/v1/rides
-  # GET /api/v1/rides.json
   def index
-    @api_v1_rides = Api::V1::Ride.all
+    @rides = Ride.all
   end
 
   # GET /api/v1/rides/1
-  # GET /api/v1/rides/1.json
   def show
   end
 
-  # GET /api/v1/rides/new
-  def new
-    @api_v1_ride = Api::V1::Ride.new
-  end
-
-  # GET /api/v1/rides/1/edit
-  def edit
-  end
-
   # POST /api/v1/rides
-  # POST /api/v1/rides.json
   def create
-    @api_v1_ride = Api::V1::Ride.new(api_v1_ride_params)
-
-    respond_to do |format|
-      if @api_v1_ride.save
-        format.html { redirect_to @api_v1_ride, notice: 'Ride was successfully created.' }
-        format.json { render :show, status: :created, location: @api_v1_ride }
-      else
-        format.html { render :new }
-        format.json { render json: @api_v1_ride.errors, status: :unprocessable_entity }
-      end
+    @ride = Ride.new(ride_params)
+    if @ride.save
+      render json: { success: true, message: 'Ride was successfully created.' }
+    else
+      render json: { success: false, message: @ride.errors.messages.flatten.join(" ")}
     end
   end
 
   # PATCH/PUT /api/v1/rides/1
-  # PATCH/PUT /api/v1/rides/1.json
   def update
-    respond_to do |format|
-      if @api_v1_ride.update(api_v1_ride_params)
-        format.html { redirect_to @api_v1_ride, notice: 'Ride was successfully updated.' }
-        format.json { render :show, status: :ok, location: @api_v1_ride }
-      else
-        format.html { render :edit }
-        format.json { render json: @api_v1_ride.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /api/v1/rides/1
-  # DELETE /api/v1/rides/1.json
-  def destroy
-    @api_v1_ride.destroy
-    respond_to do |format|
-      format.html { redirect_to api_v1_rides_url, notice: 'Ride was successfully destroyed.' }
-      format.json { head :no_content }
+    if @ride.update(ride_params)
+      render json: { success: true, message: 'Ride was successfully updated.' }
+    else
+      render json: { success: false, message: @ride.errors.messages.flatten.join(" ") }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_api_v1_ride
-      @api_v1_ride = Api::V1::Ride.find(params[:id])
+      @ride = Ride.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def api_v1_ride_params
-      params.fetch(:api_v1_ride, {})
+    def ride_params
+      params.permit(:customer_id, :cab_id, :source, :destination, :cost, :completed)
     end
 end
